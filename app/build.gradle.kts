bfa85171531caef10dfd00ae4b7e6d637c24fda1
wilthepudding <wellington18h@gmail.com>
2026-05-06 14:17:28 -0300

    Add app Gradle build file

---
+++ app/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.projeto1.miditapper"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.projeto1.miditapper"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
